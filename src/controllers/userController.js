5e096687ebb3609fc4edca331a6226b7a26e0466
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-12 17:47:16 +0500

    token veirfy

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,16 @@
 import { sendResponse } from "../utils/sendResponse.js";
 
 export const getMe = (req, res) => {
-  const data = req.client.user;
+  const { user } = req.client;
+
+  const data = {
+    id: user.id,
+    username: user.username,
+    globalName: user.globalName,
+    bot: user.bot,
+    avatarURL: user.avatarURL,
+    defaultAvatarURL: user.defaultAvatarURL,
+  };
 
   sendResponse(req, res, data);
 };
